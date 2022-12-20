8b80b013e4c916a712e8ed487e4372c811ea8608
nikolaynovikov1989 <nikolaynovikov660@gmail.com>
2022-12-21 00:46:38 +0300

    add name container

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,15 @@
 pipeline {
     agent any
+    environment {
+        cred = credentials('path')
+    }
     stages {
         stage('Check') {
             steps {
             sshagent(credentials: ['ansible_user']) {
                     sh 'pwd'
                     sh 'ls -la'
-                    sh 'ansible-playbook -i hosts playbook.yml --extra-vars "ansible_sudo_pass=12345"'
+                    sh 'ansible-playbook -i hosts playbook.yml --extra-vars "ansible_sudo_pass=${cred}"'
                 }
             }
         }
